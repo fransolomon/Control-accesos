25ef248c4a4c2a9fdf54ea5ff09a9db35c5817ef
Pablo Burgos <pburgos@acceso.com>
2017-12-21 15:27:43 +0100

    Wip web interface

--- webApp.py
+++ webApp.py
@@ -9,15 +9,22 @@ from __future__ import absolute_import, unicode_literals
 # imports
 from flask import Flask, render_template
 
-from ..data.models import Entry
+# from data.models import Entry
+from data.models import Entry, User
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    entries = Entry.filter()
+    entries = Entry.select()
     return render_template('index.html', entries=entries)
+
+
+@app.route("/users")
+def users():
+    users = User.select()
+    return render_template('users.html', users=users)
 
 
 if __name__ == "__main__":
